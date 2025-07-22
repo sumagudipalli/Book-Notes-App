574c15f7dbad9a893f47d6c5a0cc876980d89dd5
sumagudipalli <sumagudipallicme@gmail.com>
2025-07-22 23:37:12 +0530

    index.js

--- db/index.js
+++ db/index.js
@@ -1,1 +1,19 @@
+require('dotenv').config();
+const { Pool } = require('pg');
 
+// Create a connection pool to the PostgreSQL database
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Try connecting to the database and log the connection status
+pool.connect()
+  .then(() => {
+    console.log('Successfully connected to PostgreSQL database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL database', err.message);
+  });
+
+// Export the pool for usage in other parts of the app
+module.exports = pool;
